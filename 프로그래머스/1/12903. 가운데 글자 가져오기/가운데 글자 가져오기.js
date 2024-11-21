2226df99686031b50973da43c71ab190069e5791
Doyun Lee <108219121+doyn511@users.noreply.github.com>
2024-11-22 00:47:01 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,4 +1,4 @@
 function solution(s) {
     const idx = Math.round(s.length/2);
-    return s.length% 2 === 0 ? s.split('').splice(idx-1, 2).join(''): s.split('').splice(idx-1,1).join('');
+    return s.length% 2 ? s[idx-1] : s[idx-1]+s[idx]; 
 }
