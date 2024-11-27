6043d10704e7596ae878cbe1600801f9d8ebdfeb
Doyun Lee <108219121+doyn511@users.noreply.github.com>
2024-11-27 17:13:37 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 28.99 ms, Memory: 91.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,14 @@
+function solution(arr)
+{
+    const answer = [];
+    
+    for(const item of arr){
+        if(answer[answer.length-1] !== item){
+            answer.push(item);
+        }
+    }
+    
+    return answer;
+
+
+}
