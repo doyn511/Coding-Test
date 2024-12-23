d42bfc24641b00c69f004c632b1399e1f38cc07c
Doyun Lee <108219121+doyn511@users.noreply.github.com>
2024-12-23 15:20:49 +0900

    [level 1] Title: 모의고사, Time: 26.16 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -3,35 +3,16 @@ function solution(answers) {
     const second = [2,1,2,3,2,4,2,5];
     const third = [3,3,1,1,2,2,4,4,5,5];
     
+    const firstLen = first.length;
+    const secondLen = second.length;
+    const thirdLen = third.length;
+    
     let score = [{id:1, cnt:0}, {id:2, cnt:0}, {id:3, cnt:0}];
     
-    let curIdx = 0;
-    let firstIdx = 0;
-    let secondIdx = 0;
-    let thirdIdx = 0;
-    
-    while(curIdx !== answers.length){
-        if(answers[curIdx] === first[firstIdx]){
-            score[0].cnt++;
-        }
-        if(answers[curIdx] === second[secondIdx]){
-            score[1].cnt++;
-        }
-        if(answers[curIdx] === third[thirdIdx]){
-            score[2].cnt++;
-        }
-        
-        firstIdx++; secondIdx++; thirdIdx++; curIdx++;
-        
-        if(firstIdx >= first.length){
-            firstIdx = firstIdx-first.length;
-        }
-        if(secondIdx >= second.length){
-            secondIdx = secondIdx-second.length;
-        }
-        if(thirdIdx >= third.length){
-            thirdIdx = thirdIdx-third.length;
-        }
+    for(let i = 0; i <= answers.length-1; i++){
+        if(answers[i] === first[ i % firstLen]) score[0].cnt++;
+        if(answers[i] === second[ i % secondLen]) score[1].cnt++;
+        if(answers[i] === third[ i % thirdLen]) score[2].cnt++;
     }
     
     const maxScore = Math.max(score[0].cnt, score[1].cnt, score[2].cnt);
