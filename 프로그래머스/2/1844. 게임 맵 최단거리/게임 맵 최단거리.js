5856e14a420263b4093fbfdf556067099cecb71e
Doyun Lee <108219121+doyn511@users.noreply.github.com>
2025-03-13 16:57:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 19.04 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,36 +1,28 @@
 function solution(maps) {
-    let n = maps.length; //행의 크기
-    let m = maps[0].length; //열의 크기
-    const dir = [[-1,0], [1,0], [0,-1], [0,1]]; //움직일 수 있는 방향
+    const xLen = maps.length;
+    const yLen = maps[0].length;
+    const dir = [[0,1], [0,-1], [1,0], [-1,0]]; // 움직일 수 있는 방향
     
-    const bfs = () => {
-        let queue = [[0,0,1]]; //행, 열, 이동거리 순
-        maps[0][0] = 0; //이동한 거리 0으로 막기
+    const bfs = (startX, startY) => {
+        let queue = [[startX, startY, 1]];
+        maps[startX][startY] = 0;
         
-        while(queue.length > 0){
-            let [row, col, distance] = queue.shift();
+        while(queue.length >0){
+            let [x, y, dist] = queue.shift();
             
-            // row와 col이 도착지점에 도착하면 움직인 거리 리턴
-            if(row === n-1 && col === m-1) return distance;
+            if(x === xLen-1 && y === yLen-1) return dist;
             
-            
-            //상하좌우 모두 탐색
-            for(let [r,c] of dir){
-                let newRow = row+r;
-                let newCol = col+c;
+            for(const [r,c] of dir){
+                const newX = x+r;
+                const newY = y+c;
                 
-                /**
-                체크 조건
-                1. map 내부에 있는지 (newRow >=0 && newCol >= 0 && newRow < n && newCol < m)
-                2. 이동 가능한 부분인지 maps[newRow][newCol] === 1
-                */
-                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol< m && maps[newRow][newCol] === 1){
-                    queue.push([newRow,newCol, distance+1]);
-                    maps[newRow][newCol] = 0;
+                if(newX >= 0 && newX < xLen && newY >= 0 && newY < yLen && maps[newX][newY] === 1){
+                    maps[newX][newY] = 0;
+                    queue.push([newX, newY, dist+1]);
                 }
             }
         }
         return -1;
     }
-    return bfs();
+     return bfs(0,0);
 }
