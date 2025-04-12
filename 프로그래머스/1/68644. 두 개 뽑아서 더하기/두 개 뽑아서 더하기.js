0389b97c5f0786ded506b493e2f9372d48492644
Doyun Lee <108219121+doyn511@users.noreply.github.com>
2025-04-12 23:07:55 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.96 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,10 +1,15 @@
 function solution(numbers) {
     const answer = [];
-    for(let i = 0; i <= numbers.length-1; i++){
-        for(let j = i+1; j<= numbers.length-1; j++){
-            const sum = numbers[i] + numbers[j];
-            !answer.includes(sum) && answer.push(sum);
+    
+    for(let i = 0; i < numbers.length-1; i++){
+        let fixed = numbers[i];
+        
+        for(let j = i+1; j < numbers.length; j++){
+            if(!answer.includes(fixed + numbers[j])){
+                answer.push(fixed + numbers[j]);
+            }
         }
     }
+    
     return answer.sort((a,b) => a-b);
 }
