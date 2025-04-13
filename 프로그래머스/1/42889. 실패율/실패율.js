02903d41aea210ff30ba11f4c8f47b12f0dc0c16
Doyun Lee <108219121+doyn511@users.noreply.github.com>
2025-04-13 12:58:08 +0900

    [level 1] Title: 실패율, Time: 5219.29 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,34 +1,15 @@
-/**
-실패율 = 스테이지에 도달했지만 클리어 X / 스테이지에 도달한 사람
-*/
-
 function solution(N, stages) {
-    const map = new Map();
-    stages.sort((a,b) => a-b);
-    
-    stages.forEach((el) => {
-        map.has(el) ? map.set(el, map.get(el)+1) : map.set(el, 1);
-    })
-    
-    let failure = Array.from({length: N+1}, () => 0);
+    // 1부터 N까지의 실패율을 담은 배열
+    let failure = new Map();
     
     for(let i = 1; i <= N; i++){
-        // 스테이지에 도달한 유저가 없는 경우, 실패율은 0
-        if(!map.has(i)) continue;
+        const success = stages.filter((el) => el >= i).length;
+        const challenge = stages.filter((el) => el === i).length;
         
-        let reachedStage = 0;
-        for(const [stage, num] of map){
-            if(stage >= i){
-                reachedStage+= num;
-            }
-        }
-        
-        failure[i] = map.get(i) / reachedStage;    
+        failure.set(i, challenge/success);
     }
+    failure = [...failure];
     
-    // 0번째 요소 제거
-    failure.shift();
-    failure = failure.map((el, idx) => [idx+1, el]).sort((a,b) => b[1] - a[1]);
-    
-    return failure.map((el) => el[0]);   
+    const answer = failure.sort((a,b) => b[1]-a[1]).map(el => el[0]);
+    return answer;
 }
