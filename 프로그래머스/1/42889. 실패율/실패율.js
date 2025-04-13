6c17eb20b814e90c7203037db32f5472b4714692
Doyun Lee <108219121+doyn511@users.noreply.github.com>
2025-04-13 12:59:57 +0900

    [level 1] Title: 실패율, Time: 5469.48 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,15 +1,13 @@
 function solution(N, stages) {
     // 1부터 N까지의 실패율을 담은 배열
-    let failure = new Map();
+    const failure = [];
     
     for(let i = 1; i <= N; i++){
         const success = stages.filter((el) => el >= i).length;
         const challenge = stages.filter((el) => el === i).length;
         
-        failure.set(i, challenge/success);
+        failure.push([i, challenge/success]);
     }
-    failure = [...failure];
-    
-    const answer = failure.sort((a,b) => b[1]-a[1]).map(el => el[0]);
-    return answer;
+    failure.sort((a,b) => b[1]-a[1]);
+    return failure.map((el) => el[0]);
 }
