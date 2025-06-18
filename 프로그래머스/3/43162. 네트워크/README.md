7d4ccb2fafbd41c179a7c2a6ee1e5d8badaaaf08
Doyun Lee <108219121+doyn511@users.noreply.github.com>
2025-06-19 01:54:35 +0900

    [level 3] Title: 네트워크, Time: 0.44 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 0.45 ms
+메모리: 33.7 MB, 시간: 0.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 20일 01:14:55
+2025년 06월 19일 01:54:32
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,21 +1,21 @@
 function solution(n, computers) {
-    let network = 0;
     const visited = Array.from({length: n}, () => false);
-
-    const dfs = (node) => {
+    
+    const findNetwork = (node) => {
         visited[node] = true;
-        for(let i = 0; i < n; i++){
-            if(computers[node][i] === 1 && !visited[i]){
-                dfs(i);
-            }
+        
+        for(let i = 0; i < n ; i++){
+            if(computers[node][i] === 1 && !visited[i]) findNetwork(i);
         }
     }
     
-    for(let i = 0 ; i < n; i++){
+    let network = 0;
+    for(let i = 0; i < n; i++){
         if(!visited[i]){
-            dfs(i);
+            findNetwork(i);
             network++;
-        }   
+        }
     }
+    
     return network;
 }
